7cdce2ed7c6733c927bea0ae70afa245ea2a2f56
tommaso caputi <tommasocaputi85@gmail.com>
2022-12-15 17:40:46 +0100

    add rect class

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ pygame.init()
 
 size = width, height = 320*2, 240*2
 screen = pygame.display.set_mode(size)
-rect = pygame.Rect(50, 60, 20, 8)
-vel = 1
+clock = pygame.time.Clock()
+vel = 10
 x = 200
-y = 200
 
-i=0
+class rect():
+    def __init__(self):
+        pass
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -16,14 +20,11 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x>0:
        x-=vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]and x<width-20:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
-    rect.move_ip(x,y)
-
+    
     screen.fill((0,0,0))
-    pygame.draw.rect(screen, (255,255,255), (x,y,20,8))
-    pygame.display.flip()
+    pygame.draw.rect(screen, (255,255,255), (x,height-50,60,10))
+    clock.tick(60)
+    pygame.display.update()
+
