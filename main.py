92581c0963c2f272abdefa281e5be55ac5085246
tommaso caputi <tommasocaputi85@gmail.com>
2022-12-17 14:40:51 +0100

    add ball

--- main.py
+++ main.py
@@ -17,13 +17,24 @@ class rect():
             self.x -= self.vel
         if keys[pygame.K_RIGHT]and self.x<width-20:
             self.x += self.vel
+class ball():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def get_pos(self):
+        return self.x, self.y
 
+balls = [ball(width/2,height/2)]
 player = rect()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+    
     player.keys_handler()
+    
     screen.fill((0,0,0))
+    for ball in balls:
+        pygame.draw.circle(screen, (255,255,255), ball.get_pos(), 3)
     pygame.draw.rect(screen, (255,255,255), (player.x, height-50, 60, 10))
     clock.tick(60)
     pygame.display.update()
